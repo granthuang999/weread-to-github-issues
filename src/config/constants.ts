24d2dc7ad7ba1b947cc4997ba8d000b6203cee68
granthuang999 <66244223+granthuang999@users.noreply.github.com>
2025-09-10 13:04:28 +0800

    Update constants.ts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,18 +1,21 @@
 /**
- * Constants for the project
+ * Constants used in the project
  */
 
-// WeRead API Base URL
-export const WEREAD_BASE_URL = "https://weread.qq.com";
-
-// WeRead APIs
-export const NOTEBOOK_API = `${WEREAD_BASE_URL}/web/notebooks`;
-export const BOOKMARKS_API = `${WEREAD_BASE_URL}/web/book/bookmarks`;
-export const BOOKSHELF_URL = `${WEREAD_BASE_URL}/web/shelf`;
-export const BOOK_INFO_URL = `${WEREAD_BASE_URL}/web/book/info`;
-export const BOOK_PROGRESS_API = `${WEREAD_BASE_URL}/web/book/progress`;
-export const BOOK_THOUGHTS_API = `${WEREAD_BASE_URL}/web/review/list`;
-
-// Local data directory
+// Local directory to store sync state files
 export const SYNC_STATE_DIR = "data/sync-state";
+
+// WeRead API Endpoints
+export const WEREAD_BASE_URL = "https://weread.qq.com";
+export const BOOKSHELF_URL = "https://weread.qq.com/web/shelf";
+export const BOOK_INFO_URL = "https://weread.qq.com/web/book/info";
+export const BOOKMARKS_API = "https://weread.qq.com/web/book/bookmarklist";
+export const BOOK_THOUGHTS_API = "https://weread.qq.com/web/review/list";
+export const BOOK_PROGRESS_API = "https://weread.qq.com/web/book/progress";
+
+// The old, now incorrect URL for notebooks
+// export const NOTEBOOK_API = "https://weread.qq.com/web/notebooks"; 
+
+// The new, correct API endpoint for notebooks
+export const NOTEBOOK_API = "https://i.weread.qq.com/user/notebooks";
 
